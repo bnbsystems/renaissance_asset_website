d5cb49a36e46076a5e2c503438525d61a2672fc9
patrykzych1 <patzycc@gmail.com>
2022-02-09 16:20:32 +0100

    Updates

---
+++ component---src-pages-404-jsx-b3f37d6a89e9a3705ed2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkemma=self.webpackChunkemma||[]).push([[256],{8336:function(e,t,n){n.r(t);var a=n(2784),l=n(9844),u=n(6792),o=n(1544),r=n(2510),c=n(124);t.default=function(){return a.createElement(r.Z,null,a.createElement(c.Z,{title:"404 - Not found"}),a.createElement(u.W2,null,a.createElement(o.Ge.p,null,"Oh, no!",a.createElement("br",null),"You found a page that doesn't exist."," ",a.createElement(u.rU,{as:l.Link,to:"/"},"Do you want to visit the homepage?"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-b3f37d6a89e9a3705ed2.js.map
